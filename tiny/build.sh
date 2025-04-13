9e5298a6d92cee7756af91d6ed98cdeb526dc456
0xf965 <thisemaildoesnotexists@anon.erg>
2025-04-13 18:39:07 +0200

    tiny for linux/amd64

--- tiny/build.sh
+++ tiny/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cargo build --release --target aarch64-unknown-linux-gnu
-cp target/aarch64-unknown-linux-gnu/release/tiny-service ./app
+cargo build --release --target x86_64-unknown-linux-gnu
+cp target/x86_64-unknown-linux-gnu/release/tiny-service ./app
